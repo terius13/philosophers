ab043acae5ddae3618b551646232488ab6175acb
Timberly Ng <ting@c1r8s8.42singapore.sg>
2024-04-18 21:25:16 +0800

    added some utils

--- philo/includes/philo.h
+++ philo/includes/philo.h
@@ -6,19 +6,34 @@
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/17 14:23:37 by ting              #+#    #+#             */
-/*   Updated: 2024/04/17 17:57:44 by ting             ###   ########.fr       */
+/*   Updated: 2024/04/18 17:39:03 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 # define PHILO_H
 
-# define ARG_ERROR_1 "Error: Argument is not digit"
-# define ARG_ERROR_2 "Error: Negative Argument"
-# define ARG_ERROR_3 "Error: Number of Philosopher exceeds 200"
-# define ARG_ERROR_4 "Error: Incorrect number of arguments"
-
 # include <stdio.h>
 # include <stdlib.h>
+# include <stdint.h>
+# include <sys/time.h>
 
-#endif 
+// define colours
+# define RET "\033[0m"
+# define R "\033[1;31m"
+
+// checker input error msg
+# define ARG_ERROR_1 "Error: Argument is not digit / less than 1"
+# define ARG_ERROR_2 "Error: Number of Philosopher exceeds 200"
+# define ARG_ERROR_3 "Error: Incorrect number of arguments"
+
+// checker.c
+void	ft_argisdigit(char *arg);
+int		ft_pos_atoi(char *str);
+int		ft_checker(int argc, char **argv);
+
+//utils.c
+void	ft_error(char *str);
+u_int64_t   get_time(void);
+
+#endif
--- philo/srcs/utils.c
+++ philo/srcs/utils.c
@@ -1,22 +1,30 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   philo.c                                            :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: ting <ting@student.42singapore.sg>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/04/17 14:23:51 by ting              #+#    #+#             */
-/*   Updated: 2024/04/18 15:56:23 by ting             ###   ########.fr       */
+/*   Created: 2024/04/18 15:35:56 by ting              #+#    #+#             */
+/*   Updated: 2024/04/18 17:39:10 by ting             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/philo.h"
 
-int main(int argc, char **argv)
+void	ft_error(char *str)
 {
-    u_int64_t   time;
+	printf(R"%s\n"RET, str);
+	exit(1);
+}
 
-    ft_checker(argc, argv);
-    time = get_time();
-    printf("%ld\n", time);
+u_int64_t   get_time(void)
+{
+    struct timeval   time;
+
+    if (gettimeofday(&time, NULL) != 0)
+    {
+        ft_error("gettimeofday Error");
+    }
+    return((time.tv_sec * (u_int64_t)1000) + (time.tv_usec / 1000));
 }
